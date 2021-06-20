9800165b49bdd495e1154f3301893d25fe3fb34e
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-06-20 14:04:27 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,36 @@
-
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        get("/animals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/animalForm.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+        post("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            try {
+                Animal animal = new Animal(name);
+                animal.save();
+            } catch (IllegalArgumentException exception) {
+                System.out.println("Please enter an animal name.");
+            }
